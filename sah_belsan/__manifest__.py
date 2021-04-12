82079ba894a90c804d0d5db1ed207e5f1aa4e334
ayah <ayahashraf236@gmail.com>
2021-04-12 23:54:40 +0200

    [MOD]sah_belsan and theme_belsan

--- sah_belsan/__manifest__.py
+++ sah_belsan/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','tis_web_arabic','website','website_sale','website_sale_wishlist'],
+    'depends': ['base','website','website_sale','website_sale_wishlist'],
 
     # always loaded
     'data': [
