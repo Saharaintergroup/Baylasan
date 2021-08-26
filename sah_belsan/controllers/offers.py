d2470aa63e42f1172f5c9014886c6567cbef150d
ayah <ayahashraf236@gmail.com>
2021-08-26 21:24:47 +0200

    [Add Theme]

---
+++ sah_belsan/controllers/offers.py
@@ -0,0 +1,24 @@
+from odoo.addons.portal.controllers.web import Home
+from odoo import http
+from odoo.http import request
+
+from odoo.odoo.addons.http_routing.models.ir_http import slug
+
+
+class WebsiteSort(Home):
+    @http.route('/shop/offers')
+    def index(self, **kw):
+
+        Product = request.env['product.product'].with_context(bin_size=True)
+
+        offers_products = Product.search([('discount', '>', 0.0)])
+        # print(offers_products)
+        if offers_products:
+            url = "/shop/offers/%s" % slug(offers_products)
+            current_products = offers_products
+            while current_products.product_id:
+                offers_products.append(current_products.product_id)
+        print(offers_products)
+        return request.render('sah_belsan.product_offers_page', {
+            'offers_products': offers_products,
+        })
---
+++ sah_belsan/models/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class InheritProduct(models.Model):
+    _inherit = 'product.template'
+
+    discount = fields.Float("Discount (%)")
+    # old_products = fields.Char()
+
+    @api.onchange('discount')
+    def _value_sale_price(self):
+        for rec in self:
+            rec.list_price = (rec.list_price - rec.discount /100 * rec.list_price)
+
